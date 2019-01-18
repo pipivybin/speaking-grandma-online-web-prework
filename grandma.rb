5a5fa0b7d8b81bf4bac0904ea79db20a7333da73
Ming Duan <11203duan@gmail.com>
2019-01-18 19:20:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
+  if phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
