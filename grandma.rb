7584c6fc5e226026ee2dba7c7a20087f111750be
Ming Duan <11203duan@gmail.com>
2019-01-18 18:50:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(yousay)
   if yousay == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif yousay !== yousay.upcase
+  elsif yousay != yousay.upcase
   return "HUH?! SPEAK UP, SONNY!"
   else
   return  "NO, NOT SINCE 1938!"
